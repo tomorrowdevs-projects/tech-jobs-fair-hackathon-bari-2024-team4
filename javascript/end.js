6f074114886d502b5f2538aa43eb5eb2a9b7d331
Thea Mushambadze <thea.mushambadze@gmail.com>
2024-05-13 22:31:11 +0400

    add a name input on the main page

--- javascript/end.js
+++ javascript/end.js
@@ -1,29 +1,23 @@
-const username = document.getElementById('username');
-const saveScoreBtn = document.getElementById('saveScoreBtn');
-const finalScore = document.getElementById('finalScore');
-const mostRecentScore = localStorage.getItem('mostRecentScore');
+document.addEventListener('DOMContentLoaded', () => {
+    const finalScore = document.getElementById('finalScore');
+    const mostRecentScore = localStorage.getItem('mostRecentScore');
 
-const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+    finalScore.innerText = mostRecentScore;
 
-const MAX_HIGH_SCORES = 5;
+    const username = localStorage.getItem('username');
 
-finalScore.innerText = mostRecentScore;
+    const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+    const MAX_HIGH_SCORES = 5;
 
-username.addEventListener('keyup', () => {
-    saveScoreBtn.disabled = !username.value;
-});
-
-saveHighScore = (e) => {
-    e.preventDefault();
-
+    // Save the score with the username
     const score = {
         score: mostRecentScore,
-        name: username.value,
+        name: username,
     };
+
     highScores.push(score);
     highScores.sort((a, b) => b.score - a.score);
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/');
-};
+});
