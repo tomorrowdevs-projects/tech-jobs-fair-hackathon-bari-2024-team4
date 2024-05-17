a568fb66ad892a1ee2a821cf4e83ea5c6401ba5d
Vincenzo Sorrentino <vuesse2impact@gmail.com>
2024-05-17 11:09:37 +0200

    Creata struttura database

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const PORT = process.env.PORT || 5000;
 const sampleUrl =
   "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple";
 
+//DATABASE
+
 const mongoose = require("mongoose");
 const dbURI = "mongodb://localhost:27017/Quiz_Database";
 
@@ -26,7 +28,36 @@ db.once("open", () => {
   console.log("Connesso al database MongoDB.");
 });
 
+const gameSchema = new mongoose.Schema({
+  id: { type: Number },
+  users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  start: { type: Date, default: Date.now },
+  timestamp: { type: Date, default: Date.now },
+});
+
+const userSchema = new mongoose.Schema({
+  id: { type: Number },
+  username: { type: String, required: true },
+  role: { type: String, required: true },
+  created_at: { type: Date, default: Date.now },
+});
+
+const questionSchema = new mongoose.Schema({
+  id: { type: Number },
+  question: { type: String, required: true },
+  game_id: { type: mongoose.Schema.Types.ObjectId, ref: "Game" },
+  answers: [{ type: String }],
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  correct: { type: Boolean, required: true },
+  question_id: { type: mongoose.Schema.Types.ObjectId, ref: "Question" },
+  answer: { type: Boolean, required: true },
+});
+
+const Game = mongoose.model("Game", gameSchema);
+const User = mongoose.model("User", userSchema);
 const Question = mongoose.model("Question", questionSchema);
+
+//FINE DATABASE
 
 var connectedUsers = 0;
 var readyUsers = new Set();
